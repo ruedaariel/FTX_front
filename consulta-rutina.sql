f71639cdae62dba81fd043a1799dae341ca7c217
ruedaariel <ruedaar@gmail.com>
2025-11-26 19:04:32 -0300

    se borraron archivos no usados

--- consulta-rutina.sql
+++
@@ -1,7 +0,0 @@
-use pruebaftx;
-
-select * from rutina;
-
-/*listado de rutina-usuario o rutina - nada*/
-SELECT * FROM rutina r
-LEFT JOIN usuario u ON u.id = r.id_rutina;
--- consulta_pagos.sql
+++
@@ -1,8 +0,0 @@
-use pruebaftx;
-
-select * from Pagos;
-
-select * from Pagos
-LEFT JOIN usuario u ON u.id = Pagos.usuarioId;
-
-DELETE FROM Pagos WHERE id_pagos = 4;
